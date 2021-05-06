360d4de52d803de11e24bedbd5607d5be9299212
Josh <zylo.codes@gmail.com>
2021-05-06 15:53:22 -0500

    socketio101

---
+++ socketio101/server.js
@@ -0,0 +1,17 @@
+const http = require('http')
+const socketio = require('socket.io')
+
+const server = http.createServer((req, res) => res.end('I am connected'))
+
+const io = socketio(server, {
+  cors: {
+    origin: '*',
+  }
+})
+
+io.on('connection', (socket, req) => {
+  socket.emit('welcome', 'Welcome to socketio server!!')
+  socket.on('message', msg => console.log(msg))
+})
+
+server.listen(8000)
