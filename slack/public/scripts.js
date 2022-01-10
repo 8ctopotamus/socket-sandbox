8221416ec799d387cdea174b1e59239492a075c3
Josh <zylo.codes@gmail.com>
2022-01-10 15:08:43 -0600

    slack cont'd

--- slack/public/scripts.js
+++ slack/public/scripts.js
@@ -1,5 +1,8 @@
 const namespacesDiv = document.querySelector('.namespaces')
 const roomList = document.querySelector('.room-list')
+const messageForm = document.getElementById('message-form')
+const newMessageInput = document.getElementById('user-message')
+const messages = document.getElementById('messages')
 
 const SOCKET_BASE_URL = 'http://localhost:8000'
 const socket = io(SOCKET_BASE_URL)
@@ -8,13 +11,7 @@ socket.on('connect', () => {
   console.log(`${socket.id} connected!`)
 })
 
-socket.on('nsList', nsList => {
-  namespacesDiv.innerHTML = ''
-  nsList.forEach(ns => namespacesDiv.innerHTML += `
-    <div class="namespace" data-ns="${ns.endpoint}">
-      <img src="${ns.img}" alt="${ns.endpoint}">
-    </div>
-  `)
+function joinNS(endpoint) {
   const nsSocket = io(`${SOCKET_BASE_URL}/wiki`)
   nsSocket.on('nsRoomLoad', nsRooms => {
     roomList.innerHTML = ''
@@ -22,18 +19,27 @@ socket.on('nsList', nsList => {
       const glyph = room.privateRoom ? 'lock' : 'globe'
       roomList.innerHTML += `
         <li data-roomid=${room.roomId}>
-        <span class="glyphicon glyphicon-${glyph}"></span>${room.roomTitle}
+          <span class="glyphicon glyphicon-${glyph}"></span>${room.roomTitle}
         </li>    
       `
     })
   })
+}
+
+socket.on('nsList', nsList => {
+  namespacesDiv.innerHTML = ''
+  nsList.forEach(ns => namespacesDiv.innerHTML += `
+    <div class="namespace" data-ns="${ns.endpoint}">
+      <img src="${ns.img}" alt="${ns.endpoint}">
+    </div>
+  `)
+  joinNS(nsList[0].endpoint)
 })
 
 roomList.addEventListener('click', ({ target }) => {
   if (target.tagName !== 'UL') {
     const roomId = target.dataset.roomid
     console.log(roomId)
-
   }
 })
 
@@ -42,4 +48,16 @@ namespacesDiv.addEventListener('click', ({ target }) => {
     console.log(target.parentElement.dataset.ns)
   }
 })
+
+socket.on('messageFromServer', data => console.log('from server'),data)
+
+messageForm.addEventListener('submit', e => {
+  preventDefault()
+  socket.emit('newMessageToServer', newMessageInput.value)
+  newMessageInput.value = ''
+})
+
+socket.on('messageToClients', msg => messages.innerHTML += `
+  <li>${msg.text}</li>
+`)
 
